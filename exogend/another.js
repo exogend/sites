2193b7835cb3a69bc600b3095fcf53cd7fa5073c
ShannonS-DS <shannon.sandidge@gmail.com>
2016-12-25 12:03:53 -0700

    Mods

---
+++ exogend/another.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+    var Person = (function() {
+      function Person() {
+        
+      }
+      Person.prototype = {
+    
+        };
+
+      return Person;
+    }());
+}());
--- exogend/test.js
+++ exogend/test.js
@@ -1,1 +1,25 @@
 var name = 'Shannon';
+
+(function() {
+  'use strict';
+  var Animal  = (function() {
+    function Animal() {
+
+    }
+    Animal .prototype = {
+
+      };
+
+    return Animal;
+  }());
+  var Module = (function() {
+    function Module() {
+
+    }
+    Module.prototype = {
+
+      };
+
+    return Module;
+  }());
+}());
